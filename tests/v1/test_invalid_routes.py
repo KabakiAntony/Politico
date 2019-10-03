8eed7db991b772d44c420d829e7202972971d3a2
Kabaki Antony <kabaki.antony@gmail.com>
2019-10-03 21:28:44 +0300

    Adding tests for invalid routes

---
+++ tests/v1/test_invalid_routes.py
@@ -0,0 +1,19 @@
+import json 
+import unittest
+from app import create_app
+from app.api.v1.models import Party
+
+class TestInvalidRoutes(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        self.app.testing = False
+
+    def test_getting_invalid_route(self):
+        """Test getting invalid routes"""
+        response = self.client.get('api/v1/parties/')
+        self.assertEqual(response.status_code,404)
+        response = self.client.get('api/v1/anything')
+        self.assertEqual(response.status_code,404)
